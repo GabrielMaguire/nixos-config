b3327e223fa0c6b0cdea350a651916d2b42343f6
Gabriel Maguire <gabrielmaguire@proton.me>
2024-08-27 21:38:51 -0400

    Updated chicago95 package for nixpkgs PR

--- modules/chicago95.nix
+++ modules/chicago95.nix
@@ -27,9 +27,22 @@ stdenvNoCC.mkDerivation rec {
 
     mkdir -p $out/share/icons
     mv Icons/Chicago95 $out/share/icons
-
     gtk-update-icon-cache $out/share/icons/Chicago95
+
+    mkdir -p $out/share/sddm/themes
+    tar -xzf KDE/SDDM/Chicago95.tar.gz -C "$out/share/sddm/themes/"
+
+    mkdir -p $out/share/fonts
+    cp Fonts/vga_font/* $out/share/fonts
 
     runHook postInstall
   '';
+
+  meta = with lib; {
+    description = "Windows 95 theme for linux";
+    homepage = "https://github.com/grassmunk/Chicago95";
+    license = with licenses; [ gpl3Plus mit ];
+    platforms = platforms.linux;
+    maintainers = with maintainers; [ GabrielMaguire ];
+  };
 }
